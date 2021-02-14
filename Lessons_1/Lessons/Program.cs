a728816967f6aee988368c040b1a5f431db78a12
Fatal_fury <fury15@yandex.ru>
2021-02-14 05:00:28 +0500

    add branch lessons_1

---
+++ Lessons_1/Lessons/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hellow hren");
+
+            //bool res = true && true;
+            //int know = 0b011111;
+
+            //int back = 0b011010;
+            //int front = 0b011010;
+            //int des = 0b0110000;
+            //int backprof = know & back;
+
+            //string str = "human";
+            //if (str == "human")
+            //{
+            //    Console.WriteLine(str);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not human");
+            //}
+            Console.Write("What's your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+
+
+        }
+
+    }
+}
