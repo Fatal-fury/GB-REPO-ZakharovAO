e4b6a187b526b0d9cee5d4d9f16d629a848991db
Fatal_fury <fury15@yandex.ru>
2021-02-16 22:44:54 +0500

    lesson_2

---
+++ work_2/les_2/les_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace les_2
+{
+    class Program
+    {
+
+        [Flags]
+        public enum Month
+        {
+            January  = 1,
+            February = 2,
+            March    = 3,
+            April    = 4,
+            May      = 5,
+            June     = 6,
+            July     = 7,
+            August   = 8,
+            September= 9,
+            October  = 10,
+            November = 11,
+            December = 12,
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите номер месяца");
+            int numberMonth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((Month)numberMonth);
+        }
+    }
+}
---
+++ work_2/les_3/les_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace les_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Внимание!!!! Проверка на четность, введенного Вами числа!");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Число четное");
+                
+            }
+            else { Console.WriteLine("Число не четное"); }
+        }
+    }
+}
