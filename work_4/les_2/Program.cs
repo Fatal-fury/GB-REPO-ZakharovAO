d28d8f007dde84b75a3dfc1c05a6b286349fdeb5
Fatal_fury <fury15@yandex.ru>
2021-02-28 04:18:10 +0500

    lesson_4

---
+++ work_4/les_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace les_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи числа через пробел");
+            string numbers = Console.ReadLine();
+            Console.WriteLine(sum(numbers));
+        }
+
+        static int sum(string numbers)
+        {
+            int number = 0;
+            string s = "0";
+            for (int i = 0;i<numbers.Length; i++)
+            {
+                s = "0";
+                while ((i < numbers.Length)&&(numbers[i]!=' '))
+                {
+                    s = s + numbers[i];       
+                    i++;
+                }
+                number += Convert.ToInt32(s);
+            }
+            return number;
+        }
+
+    }
+}
---
+++ work_4/les_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace les_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер элемента числа Фиббоначи: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Число Фиббоначи для элемент n={n} равно: {NumFibbonachi(n)}");
+            Console.ReadKey();
+        }
+
+        static int NumFibbonachi(int n)
+        {
+            if (n ==0)
+            {
+                return 0;
+            }
+            else if (n==1)
+            {
+                return 1;
+
+             }
+            else 
+            {
+                return NumFibbonachi(n-2) + NumFibbonachi(n - 1);
+            }
+        }
+    }
+}
