75bf2b2d8b23c54b8551d30c1d0f4eef65009829
Fatal_fury <fury15@yandex.ru>
2021-03-02 23:20:21 +0500

    les_5

---
+++ work_5/les_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace les_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime timeNow = DateTime.Now;
+
+
+            string filename = "lesson2";
+            File.WriteAllText(filename, $"{timeNow:T}");
+            string text = File.ReadAllText(filename);
+
+            Console.WriteLine(text);
+        }
+    }
+}
