e7a7d5d10fb4a754099708dd2cf080a0a8d6f976
Fatal_fury <fury15@yandex.ru>
2021-03-17 19:15:43 +0500

    les_8

---
+++ work_8/les_8/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace les_8
+{
+    class Program
+    {
+
+
+        static Process[] process { get; set; }
+        public static int n;
+
+        static void GetProcessList()
+        {
+            process = Process.GetProcesses();
+        }
+        static void Main(string[] args)
+        {GetProcessList();
+            viewproces();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("ВВедите имя процесса или PID процесса, который необходимо завершить");
+            Console.ReadLine();
+            
+            ReadLineProc();
+            KillProc();
+
+        }
+        public static void viewproces()
+        {
+                        
+            foreach (var item in process)
+            {
+                Console.WriteLine($"{item.Id}\t{item.ProcessName}");
+            }
+            
+        }
+        public static void ReadLineProc()
+        {
+          // int n;
+            string Proc = Console.ReadLine();
+            if (int.TryParse(Proc,out n)
+               
+            );
+            
+            //else return Proc;      
+        }
+        public static void KillProc()
+        {
+            foreach (var item in process)
+            {
+                if (item.Id==n)
+                {
+                    item.Kill();
+                }
+                
+            }
+            
+
+        }
+    }
+
+
+
+
+
+
+}
